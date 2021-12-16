27ad55fdfcdef7de7cd10ce15180d2cb4e1d1e84
Markus Gutschke <gutschke@users.noreply.github.com>
2021-12-16 00:49:00 -0800

    Add compatibility with Ubuntu on Raspberry Pi CM4

--- overlayfs.sh
+++ overlayfs.sh
@@ -1,38 +1,48 @@
 #!/bin/bash
 
 apt install initramfs-tools busybox-static
+[ -r /usr/share/initramfs-tools/hooks/zz-busybox-initramfs ] &&
+sed 's/^\(BB_BIN=\).*/\1\/usr\/bin\/busybox/' /usr/share/initramfs-tools/hooks/zz-busybox-initramfs >/usr/share/initramfs-tools/hooks/zzz-busybox-initramfs &&
+chmod 755 /usr/share/initramfs-tools/hooks/zzz-busybox-initramfs
 
 if ! grep overlay /etc/initramfs-tools/modules > /dev/null; then
   echo overlay >> /etc/initramfs-tools/modules
 fi
 
 cp overlay /etc/initramfs-tools/scripts
-mount -o remount,rw /boot
+
+# Different distributions place the boot files into slightly different
+# locations. So, make an effort to automatically locate them.
+boot="$(awk '$2 ~ /^\/boot/ { print $2; exit }' /proc/mounts)"
+[ -n "${boot}" -a -r "${cfg}/config.txt" ] || cfg="/boot"
+cfg="$(readlink -f "${cfg}/config.txt" 2>/dev/null)"
+cfg="$(dirname "${cfg:-/boot/config.txt}")"
+
+mount -o remount,rw "${boot}"
 
 # This is needed for the u-boot
-if [ -r /boot/armbianEnv.txt ]; then
-  if ! grep boot=overlay /boot/armbianEnv.txt > /dev/null; then
+if [ -r "${cfg}/armbianEnv.txt" ]; then
+  if ! grep boot=overlay "${cfg}/armbianEnv.txt" > /dev/null; then
     sed -i '/^extraargs=/{
               s/boot=overlay//g;s/  */ /g;s/=/&boot=overlay /;s/ *$//
 	      :1;N;b1
             }
 	    $a\
-extraargs=boot=overlay' /boot/armbianEnv.txt
+extraargs=boot=overlay' "${cfg}/armbianEnv.txt"
   fi
 fi
 
 update-initramfs -c -k $(uname -r)
 
-# This is needed for the Raspberry Pi bootloader
-if [ -r /boot/config.txt ]; then
-  mv /boot/initrd.img-$(uname -r) /boot/initrd7.img
+# This is needed for the Raspberry Pi bootloader.
+if [ -r "${cfg}/config.txt" ]; then
+# mv "${cfg}/initrd.img-$(uname -r)" "${cfg}/initrd7.img"
+# sed -e "s/initramfs.*//" -i "${cfg}/config.txt"
+# echo initramfs initrd7.img >> "${cfg}/config.txt"
 
-  sed -e "s/initramfs.*//" -i /boot/config.txt
-  echo initramfs initrd7.img >> /boot/config.txt
-
-  sed -e 's/.*/ & /;:1;s/ boot=overlay / /g;t1;s/ \+/ /g;s/^ //;s/ $//' /boot/cmdline.txt > /boot/cmdline.txt.orig &&
-  sed -e "s/.*/boot=overlay &/" /boot/cmdline.txt.orig >/boot/cmdline.txt.overlay &&
-  cp /boot/cmdline.txt.overlay /boot/cmdline.txt
+  sed -e 's/.*/ & /;:1;s/ boot=overlay / /g;t1;s/ \+/ /g;s/^ //;s/ $//' "${cfg}/cmdline.txt" > "${cfg}/cmdline.txt.orig" &&
+  sed -e "s/.*/boot=overlay &/" "${cfg}/cmdline.txt.orig" >"${cfg}/cmdline.txt.overlay" &&
+  cp "${cfg}/cmdline.txt.overlay" "${cfg}/cmdline.txt"
 fi
 
 cp overctl /usr/local/sbin
